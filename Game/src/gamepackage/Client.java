d7895acdb0ce6deffa68eabd8d83d0167a1015dd
janku <janku@DESKTOP-PJ81TJI>
2018-12-04 12:57:40 +0100

    Every player recognizes his own color

--- Game/src/gamepackage/Client.java
+++ Game/src/gamepackage/Client.java
@@ -2,7 +2,10 @@ package gamepackage;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
@@ -23,6 +26,7 @@ public class Client {
 	BoardImage boardImage;
 	JCheckBox p2,p3,p4,p6;
 	ButtonGroup players;
+
 	
 	public Client() {
 		
@@ -32,8 +36,7 @@ public class Client {
 		boardImage  = new BoardImage();
 		
 		communicator.setEditable(false);	// bedzie wyswietlal komunikaty o zlym ruchu itp.
-		communicator.setBackground(new Color(255,217,179));
-		
+		communicator.setFont(new Font("Segoe Script",Font.BOLD,15));
 		clientWindow.setLayout(new BorderLayout());
 		clientWindow.setSize(800, 600);
 		clientWindow.getContentPane().setBackground(new Color(255,153,51));		
@@ -44,46 +47,40 @@ public class Client {
 		
 		buttons.setLayout(new BoxLayout(buttons,BoxLayout.Y_AXIS));	// beda tu rozne przyciski jak connect itp
 		buttons.setBounds(600, 0, 150, 665);
-		buttons.add(new JButton("Join game"));
-		buttons.add(new JButton("Host game"));
-		buttons.setBackground(new Color(255,217,179));
-		
-		p2 = new JCheckBox("2 Players");
-	    p2.setSelected(true);
-	    p3 = new JCheckBox("3 Players");
-	    p3.setSelected(false);
-	    p4 = new JCheckBox("4 Players");
-	    p4.setSelected(false);
-	    p6 = new JCheckBox("6 Players");
-	    p6.setSelected(false);
-	    players = new ButtonGroup();
-	    players.add(p2);
-	    players.add(p3);
-	    players.add(p4);
-	    players.add(p6);
-	    buttons.add(p2);
-	    buttons.add(p3);
-	    buttons.add(p4);
-	    buttons.add(p6);
+		buttons.add(new JButton("END YOUR TURN"));
+
 	    buttons.add(communicator);
 	    clientWindow.setVisible(true);
 	}
 	
-	public static void main(String[] args) {
+	private void setMyColor(Color color) {
+		communicator.setBackground(color);
+	}
+	public static void main(String[] args) throws InterruptedException {
 
 				int numberOfPlayers = 0;
+			    Color color = Color.WHITE;
 				Connector connector = new Connector();
+				
 				connector.out.println("CONNECT");
+				
 				try {
 					numberOfPlayers = Integer.parseInt(connector.in.readLine());
+					color = new Color(Integer.parseInt(connector.in.readLine()));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+				
 				Client client = new Client(); 
-				Board board = new Board(numberOfPlayers);
-				connector.setBoardImage(client.boardImage);
+				Board board = new Board(numberOfPlayers,client.boardImage);
+				
+				board.setConnector(connector);
+				connector.setBoardImage(board,client.communicator);
 				client.boardImage.setBoard(board);
-				client.boardImage.setConnector(connector);
+				client.setMyColor(color);
+				client.boardImage.setConnector(connector);			
 				connector.play();
-				}
+
+		}
+		
 	}

252d289e48407da0e6edaceaa7efb2abb150ec45
Piotr <piotrsz292@gmail.com>
2018-12-15 17:27:04 +0100

    Improved tests for states

--- Game/src/tests/StateTest.java
+++ Game/src/tests/StateTest.java
@@ -1,13 +1,10 @@
 package tests;
 
 import static org.junit.Assert.assertArrayEquals;
-
-import java.util.ArrayList;
-
+import static org.junit.Assert.assertEquals;
+import java.awt.Color;
 import org.junit.Test;
-
 import gamepackage.Board;
-import gamepackage.ColorCircle;
 import states.State;
 import states.StateController;
 
@@ -21,6 +18,12 @@ public class StateTest {
 		assertArrayEquals(controller.getState().setDestinationForPlayer(1),State.player);
 		Board board = new Board(2,null);
 		controller.getState().setAreasForPlayers(board);
+		assertEquals(board.getCircles().get(0).getColor(),Color.RED);
+		assertEquals(board.getCircles().get(10).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(22).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(98).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(110).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(120).getColor(),Color.GREEN);
 	}
 	@Test
 	public void testForThreePlayers() {
@@ -31,6 +34,12 @@ public class StateTest {
 		assertArrayEquals(controller.getState().setDestinationForPlayer(1),State.player1);
 		assertArrayEquals(controller.getState().setDestinationForPlayer(2),State.player2);
 		controller.getState().setAreasForPlayers(board);
+		assertEquals(board.getCircles().get(0).getColor(),Color.RED);
+		assertEquals(board.getCircles().get(10).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(22).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(98).getColor(),Color.BLUE);
+		assertEquals(board.getCircles().get(110).getColor(),Color.GREEN);
+		assertEquals(board.getCircles().get(120).getColor(),Color.WHITE);
 	}
 	@Test
 	public void testForFourPlayers() {
@@ -42,6 +51,12 @@ public class StateTest {
 		assertArrayEquals(controller.getState().setDestinationForPlayer(2),State.player1);
 		assertArrayEquals(controller.getState().setDestinationForPlayer(3),State.player2);
 		controller.getState().setAreasForPlayers(board);
+		assertEquals(board.getCircles().get(0).getColor(),Color.WHITE);
+		assertEquals(board.getCircles().get(10).getColor(),Color.RED);
+		assertEquals(board.getCircles().get(22).getColor(),Color.GREEN);
+		assertEquals(board.getCircles().get(98).getColor(),Color.CYAN);
+		assertEquals(board.getCircles().get(110).getColor(),Color.BLUE);
+		assertEquals(board.getCircles().get(120).getColor(),Color.WHITE);
 	}
 	@Test
 	public void testForSixPlayers() {
@@ -55,5 +70,11 @@ public class StateTest {
 		assertArrayEquals(controller.getState().setDestinationForPlayer(4),State.player2);
 		assertArrayEquals(controller.getState().setDestinationForPlayer(5),State.player4);
 		controller.getState().setAreasForPlayers(board);
+		assertEquals(board.getCircles().get(0).getColor(),Color.RED);
+		assertEquals(board.getCircles().get(10).getColor(),Color.YELLOW);
+		assertEquals(board.getCircles().get(22).getColor(),Color.GREEN);
+		assertEquals(board.getCircles().get(98).getColor(),Color.PINK);
+		assertEquals(board.getCircles().get(110).getColor(),Color.BLUE);
+		assertEquals(board.getCircles().get(120).getColor(),Color.CYAN);
 	}
 }

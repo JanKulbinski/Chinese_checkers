ab6b89119cf34af74c4ee49fd178b0a9e3bc81d4
JanKulbinski <jan.kulbinski2@gmail.com>
2018-12-18 21:10:20 +0100

    Bot testing- bug fixed

--- Game/src/tests/BotTests.java
+++ Game/src/tests/BotTests.java
@@ -2,7 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Ignore;
+import org.junit.Test;
+
 import gameServer.Bot;
 import gameServer.Game;
 import gameServer.Player;
@@ -10,15 +15,21 @@ import gamepackage.Board;
 
 public class BotTests {
 
-	@Ignore
+	@Test
 	public void test() throws InterruptedException {
 		Game game = new Game(2);
-		Board initialBoard = game.getBoard();
-		Player bot = new Bot(game,1,2);
-		bot.sendTurn(1);
-		bot.start();
-		Board changedBoard = game.getBoard();
-		assertNotEquals(initialBoard.getCircles().get(111).getColor(),changedBoard.getCircles().get(111).getColor());// eh czemu
+		Color colorBeforeMove=game.getBoard().getCircles().get(111).getColor();
+		Player bot = new Bot(game,0,2);
+		Player bot2 = new Bot(game,1,2);
+		Player[] players = new Player[2];
+		players[0] = bot;
+		players[1] = bot2;
+		game.setPlayers(players);
+		bot2.sendTurn(1);
+		bot2.start();
+		TimeUnit.SECONDS.sleep(1);
+		Color colorAfterMove= game.getBoard().getCircles().get(111).getColor();
+		assertNotEquals(colorBeforeMove,colorAfterMove);
 	}
 
 }
